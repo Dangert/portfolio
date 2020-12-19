d2eb2dd4bfc9c31ca57de74ba94d0ebf6fa224ab
Dangert <dangy4816@gmail.com>
2020-12-19 15:41:51 +0200

    adding some of the projectCard elements

--- src/App.js
+++ src/App.js
@@ -16,12 +16,16 @@ function App() {
         </div>
         <img className='below' alt='below' src={below}/>
       </div>
-      <ProjectCard title={"title"} text={"this is a project"} images={[
+      <ProjectCard title={"Title"} text={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
+      images={[
         pianoApp1,
         pianoApp2,
         pianoApp3,
       ]}
-      buttons={[]}/>
+      links={[
+        {name: 'website', href: 'https://guess-the-note.herokuapp.com/'},
+        {name: 'UI code', href: 'https://github.com/Dangert/piano-app'}]}
+      tools={[{name: 'React', class: 'react'}, {name: 'Node.js', class: 'nodejs'}, {name: 'PostreSQL', class: 'postgres'}]}/>
     </div>
   );
 }
--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,19 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 import './ProjectCard.css';
 
 function ProjectCard(props) {
-  const { title, text, images, buttons } = props;
+  const { title, text, images, links } = props;
+  const [mouseIsOver, setMouseOver] = useState(false);
+
   return (
-    <div className="project-card">
+    <div className="project-card" onMouseEnter={() => {setMouseOver(true)}} onMouseLeave={() => {setMouseOver(false)}}>
       <Carousel>
       {
-        images.map(img => {
-          return <div><img src={img}/></div>
+        images.map((img, i) => {
+          return <div key={i}><img alt={title} className={mouseIsOver ? "blur" : ""} src={img}/></div>
         })
       }
       </Carousel>
+      {
+        mouseIsOver ?
+          <div className="layer">
+            <h2>{title}</h2>
+            <p>{text}</p>
+            <div className="links">
+              {
+                links.map((link, j) => {
+                  return <a key={j} href={link.href}>{'view ' + link.name}</a>
+                })
+              }
+            </div>
+          </div>
+        : null
+      }
     </div>
   );
 }
