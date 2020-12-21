a880f203e458096a06f029267395f42aefbb8440
Dangert <dangy4816@gmail.com>
2020-12-21 17:46:45 +0200

    auto display mode and writing piano card

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import pianoApp2 from './assets/piano-app/2.png';
 import pianoApp3 from './assets/piano-app/3.png';
 
 function App() {
+
   return (
     <div className="App">
       <div className="name-display">
@@ -16,7 +17,8 @@ function App() {
         </div>
         <img className='below' alt='below' src={below}/>
       </div>
-      <ProjectCard title={"Title"} text={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
+
+      <ProjectCard title={"Guess the Note"} text={"A cool game for those who want to challenge their relative pitch!\nEach round, the player is given a random musical note (a pitch) and the sooner they get it, the more points they win. They can go for harder levels and win even more points.\nThe top 10 scorers appear in the prestigious \"Hall of Fame\" table."}
       images={[
         pianoApp1,
         pianoApp2,
@@ -24,8 +26,11 @@ function App() {
       ]}
       links={[
         {name: 'website', href: 'https://guess-the-note.herokuapp.com/'},
-        {name: 'UI code', href: 'https://github.com/Dangert/piano-app'}]}
-      tools={[{name: 'React', class: 'react'}, {name: 'Node.js', class: 'nodejs'}, {name: 'PostgreSQL', class: 'postgres'}]}/>
+        {name: 'UI code', href: 'https://github.com/Dangert/piano-app'},
+        {name: 'server code', href: 'https://github.com/Dangert/piano-api'}
+      ]}
+      tools={[{name: 'ReactJS', class: 'react'}, {name: 'Node.js', class: 'nodejs'}, {name: 'PostgreSQL', class: 'postgres'}]}/>
+
     </div>
   );
 }
--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,15 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 import './ProjectCard.css';
+import AOS from 'aos';
+import "aos/dist/aos.css";
+//import ReactCSSTransitionGroup from 'react-transition-group';
 
 function ProjectCard(props) {
   const { title, text, images, links, tools } = props;
   const [mouseIsOver, setMouseOver] = useState(false);
 
+  useEffect(() => {
+    AOS.init({ duration: 2000 });
+  }, []);
+
   return (
-    <div className="project-card" onMouseEnter={() => {setMouseOver(true)}} onMouseLeave={() => {setMouseOver(false)}}>
-      <Carousel>
+    <div data-aos="fade-up" className="project-card" onMouseEnter={() => {setMouseOver(true)}} onMouseLeave={() => {setMouseOver(false)}}>
+      <Carousel autoPlay interval="2500" infiniteLoop showStatus={false} showThumbs={false}>
       {
         images.map((img, i) => {
           return <div key={i}><img alt={title} className={mouseIsOver ? "blur" : ""} src={img}/></div>
